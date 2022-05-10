db6d684610a63f51a9a64ab5506bff5bc1e9da98
nickcrowney <nicholascrowney@gmail.com>
2022-05-10 16:30:53 +0200

    added primitive pop up

--- client/pages/diems.tsx
+++ client/pages/diems.tsx
@@ -3,8 +3,18 @@ import Head from 'next/head';
 import Tile from '../components/Tile';
 import Diem from '../components/Diem';
 import styles from '../styles/Home.module.css';
+import Popup from 'reactjs-popup';
+import { useForm } from 'react-hook-form';
+import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
 
 const Diems: NextPage = () => {
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState('');
+
+  useEffect(() => {}, [setData]);
+  const currentDate = dayjs().toISOString(); //.format('YYYY-MM-DDTHH:mm:ss.SSSZ');
+
   return (
     <div>
       <Head>
@@ -14,6 +24,33 @@ const Diems: NextPage = () => {
       </Head>
 
       <main className={styles.container}>
+        <div>
+          <Popup
+            className={styles.plusdiem}
+            trigger={<button> ➕ </button>}
+            position="right top"
+          >
+            <div className={styles.newdiem}>
+              <div>{data}</div>
+
+              <form
+                onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}
+                className={styles.input}
+              >
+                <input {...register('diem-name')} placeholder="Diem Name..." />
+                <input
+                  type="date"
+                  min={currentDate}
+                  name="date"
+                  {...register('date', { required: true })}
+                ></input>
+                <input type="submit" />
+              </form>
+
+              {/* <button>Click here</button> */}
+            </div>
+          </Popup>
+        </div>
         <div className={styles.tiles}>
           <Tile />
           <Tile />
