1a8101653c75a806e07d023d36f113bb67780b4c
Alastair Fletcher <alastair.fletcher02@gmail.com>
2022-05-11 18:49:24 +0200

    update Diems

--- client/pages/diems.tsx
+++ client/pages/diems.tsx
@@ -1,54 +1,42 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import Tile from '../components/Tile';
 import Diem from '../components/Diem';
 import Nav from '../components/Nav';
 import styles from '../styles/Home.module.css';
-
-import { useForm } from 'react-hook-form';
-
 import { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
-import guillems from '../services/ApiServices';
+import hooks from '../services/ApiServices';
 import { async } from '@firebase/util';
-// (async function func () {
-//   await props.getUsers()
-// return
-// })();
+
 const Diems: NextPage = () => {
-  const { register, handleSubmit } = useForm();
   const [data, setData] = useState('');
   const [mainDiem, setDiem] = useState('');
   const [allDiems, setAllDiems] = useState([]);
-  const [currentDiem, setCurrentDiem] = useState({ title: 'Select Diem' });
+  const [currentDiem, setCurrentDiem] = useState({
+    title: 'Select Diem',
+  });
   const [users, setUsers] = useState([]);
 
   useEffect(() => {}, [data]);
   useEffect(() => {
-    guillems
+    hooks
       .getUsers()
       .then((res) => setUsers(res))
       .catch((error) => console.log(error));
   }, []);
 
   useEffect(() => {
-    guillems
+    hooks
       .getDiems()
       .then((res) => {
         setAllDiems(res);
         setCurrentDiem(res[0]);
       })
-
-      // .then((res) => setCurrentDiem(allDiems[0]))
-      //   !currentDiem
-      //     ? (res) => setCurrentDiem(res[0])
-      //     : console.log('current diem unchanged')
-      // )
       .catch((error) => console.log(error));
   }, []);
-  console.log(guillems.getDiems(), 'AWAITED DIEMS');
-  const currentDate = dayjs().toISOString(); //.format('YYYY-MM-DDTHH:mm:ss.SSSZ');
-  const currentUser = 1;
+
+  console.log(hooks.getDiems(), 'AWAITED DIEMS');
+  // const currentDate = dayjs().toISOString(); //.format('YYYY-MM-DDTHH:mm:ss.SSSZ');
   return (
     <div>
       <Nav users={users} setUsers={setUsers} />
