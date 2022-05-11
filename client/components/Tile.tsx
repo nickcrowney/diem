f30452c00b7127aaae292bdc137e28e4e8723273
Alastair Fletcher <alastair.fletcher02@gmail.com>
2022-05-11 11:53:25 +0200

    update tile component

--- client/components/Tile.tsx
+++ client/components/Tile.tsx
@@ -4,37 +4,33 @@ import Image from 'next/image';
 import mypic from '../public/images/amir-seilsepour-Pc0ToyoR5Xo-unsplash.jpg';
 import mypic2 from '../public/images/art-hauntington-jzY0KRJopEI-unsplash.jpg';
 import mypic3 from '../public/images/christian-buehner-6YQmQgcQ0VA-unsplash.jpg';
-//
 import styles from './Tile.module.css';
 
-const Tile: React.FunctionComponent = () => {
+const Tile: React.FunctionComponent = ({ setDiem, mainDiem }) => {
   const divClickedHandler = (event: React.MouseEvent<HTMLDivElement>) => {
-    const div = event.currentTarget;
-    console.log(
-      'Element name: ',
-      div.tagName,
-      'Width: ',
-      div.clientWidth,
-      'Height: ',
-      div.clientHeight
-    );
+    console.log(mainDiem);
   };
+
+  const pics = [mypic, mypic2, mypic3];
+  const date = '22nd May 2022';
+  const event = "Tim's BBQ";
 
   return (
     <div className={styles.tile} onClick={divClickedHandler}>
       <div className={styles.tile__profilePics_container}>
-        <div className={styles.tile__profilePic}>
-          <Image src={mypic} alt="Picture of the author" />
-        </div>
-        <div className={styles.tile__profilePic}>
-          <Image src={mypic2} alt="Picture of the author" />
-        </div>
-        <div className={styles.tile__profilePic}>
-          <Image src={mypic3} alt="Picture of the author" />
-        </div>
+        {pics.map((pic) => {
+          return (
+            <div className={styles.tile__profilePic}>
+              <Image src={pic} alt="Picture of the author" />
+            </div>
+          );
+        })}
       </div>
-      <h1>22nd May 2022</h1>
-      <h2>John's stag party</h2>
+
+      <div className={styles.tile__info}>
+        <h1>{event}</h1>
+        <h2>{date}</h2>
+      </div>
     </div>
   );
 };
