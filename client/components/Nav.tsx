7ee74e79b19d250c71bbfd472f6c7763345d0f45
Alastair Fletcher <alastair.fletcher02@gmail.com>
2022-05-11 11:52:32 +0200

    update nav component

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import styles from './Nav.module.css';
+import Image from 'next/image';
+import plus from '../public/images/more.png';
+import menu from '../public/images/menu.png';
 
 const Nav = () => {
-  return <div className={styles.navContainer}></div>;
+  const addClick = () => {
+    console.log('fegregreg');
+  };
+
+  return (
+    <div className={styles.navContainer}>
+      <div className={styles.navBar}>
+        <button type="button" onClick={addClick}>
+          <Image src={plus} height="50" width="50" />
+        </button>
+        <button type="button" onClick={addClick}>
+          <Image src={menu} height="50" width="45" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
