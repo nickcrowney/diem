c701d1d75849d5bf2ef484efd72d3e3aea5c23c5
Alastair Fletcher <alastair.fletcher02@gmail.com>
2022-05-11 11:52:14 +0200

    update diem component

--- client/components/Diem.tsx
+++ client/components/Diem.tsx
@@ -7,28 +7,25 @@ import mypic3 from '../public/images/christian-buehner-6YQmQgcQ0VA-unsplash.jpg'
 //
 import styles from './Diem.module.css';
 
-const Diem: React.FunctionComponent = () => {
+const Diem: React.FunctionComponent = ({ mainDiem }) => {
+  const pics = [mypic, mypic2, mypic3];
+  const date = '22nd May 2022';
+  const event = "Tim's BBQ";
   return (
     <div className={styles.diem}>
       <div className={styles.diem__infobar}>
-        <div className={styles.diem__buttons_container}>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            +
-          </button>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            -
-          </button>
+        <div className={styles.tile__info}>
+          <h1>{event}</h1>
+          <h2>{date}</h2>
         </div>
         <div className={styles.diem__profilePics_container}>
-          <div className={styles.diem__profilePic}>
-            <Image src={mypic} alt="Picture of the author" />
-          </div>
-          <div className={styles.diem__profilePic}>
-            <Image src={mypic2} alt="Picture of the author" />
-          </div>
-          <div className={styles.diem__profilePic}>
-            <Image src={mypic3} alt="Picture of the author" />
-          </div>
+          {pics.map((pic) => {
+            return (
+              <div className={styles.diem__profilePic}>
+                <Image src={pic} alt="Picture of the author" />
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
