b1762be7aa6998ed8febbf800fd6856dc6309c1f
Alastair Fletcher <alastair.fletcher02@gmail.com>
2022-05-11 18:48:53 +0200

    update Nav

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import styles from './Nav.module.css';
 import Image from 'next/image';
-import more from '../public/images/more.png';
 import menu from '../public/images/menu.png';
 import PopNewDiem from './PopNewDiem';
-import guillems from '../services/ApiServices';
+import hooks from '../services/ApiServices';
 import mypic from '../public/images/filipe-almeida-XHpgMMiOvuM-unsplash.jpg';
 
 const Nav = ({ users, setUsers }) => {
-  const addClick = () => {
-    // console.log('fegregreg');
-  };
   // let use = [];
   // guillems.getUsers().then((res) => (use = res));
   // console.log(use, 'USE');
@@ -19,31 +15,18 @@ const Nav = ({ users, setUsers }) => {
     <div className={styles.navContainer}>
       <div className={styles.navBar}>
         <div className={styles.addDiem}>
-          <button type="button" onClick={addClick}>
-            <Image src={more} height="40" width="40" />
+          <button type="button">
+            <PopNewDiem />
           </button>
         </div>
 
         <div className={styles.nav__end}>
-          <button type="button" onClick={addClick}>
+          <button type="button">
             <Image src={menu} height="32" width="32" />
           </button>
-
           <div className={styles.nav__profilePic}>
             <Image src={mypic} alt="Picture of the author" />
           </div>
-        </div>
-        <button type="button" onClick={addClick}>
-          <PopNewDiem />
-        </button>
-        <button type="button" onClick={addClick}>
-          <Image src={menu} height="50" width="45" />
-        </button>
-        <div>
-          {' '}
-          {users.map((el) => {
-            return <div>{el.userPhoto}</div>;
-          })}
         </div>
       </div>
     </div>
