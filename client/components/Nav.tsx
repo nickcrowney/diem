492217756ee7f76ea73cccd8c7f1444daeac7916
Alastair Fletcher <alastair.fletcher02@gmail.com>
2022-05-11 16:29:20 +0200

    update nav

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from './Nav.module.css';
 import Image from 'next/image';
-import plus from '../public/images/more.png';
+import more from '../public/images/more.png';
 import menu from '../public/images/menu.png';
+import mypic from '../public/images/amir-seilsepour-Pc0ToyoR5Xo-unsplash.jpg';
 
 const Nav = () => {
   const addClick = () => {
@@ -12,12 +13,21 @@ const Nav = () => {
   return (
     <div className={styles.navContainer}>
       <div className={styles.navBar}>
-        <button type="button" onClick={addClick}>
-          <Image src={plus} height="50" width="50" />
-        </button>
-        <button type="button" onClick={addClick}>
-          <Image src={menu} height="50" width="45" />
-        </button>
+        <div className={styles.addDiem}>
+          <button type="button" onClick={addClick}>
+            <Image src={more} height="40" width="40" />
+          </button>
+        </div>
+
+        <div className={styles.nav__end}>
+          <button type="button" onClick={addClick}>
+            <Image src={menu} height="32" width="32" />
+          </button>
+
+          <div className={styles.nav__profilePic}>
+            <Image src={mypic} alt="Picture of the author" />
+          </div>
+        </div>
       </div>
     </div>
   );
