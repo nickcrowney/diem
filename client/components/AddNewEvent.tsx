b88b55af1b2f813b7b42a5d201365c7a5853f5ad
nickcrowney <nicholascrowney@gmail.com>
2022-05-12 10:53:14 +0200

    events look more sensible

--- client/components/AddNewEvent.tsx
+++ client/components/AddNewEvent.tsx
@@ -54,7 +54,7 @@ const AddNewEvent = ({ currentDiem }) => {
             ></input> */}
             <input type="submit" />
           </form>
-
+          <div></div>
           {/* <button>Click here</button> */}
         </div>
       </Popup>
