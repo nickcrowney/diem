4367c448e8390e2620b273b55e7820d9aaed19d4
Alastair Fletcher <alastair.fletcher02@gmail.com>
2022-05-14 21:40:16 +0200

    remove profile pics from Tile component

--- client/components/Tile.tsx
+++ client/components/Tile.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import Image from 'next/image';
-import mypic from '../public/images/amir-seilsepour-Pc0ToyoR5Xo-unsplash.jpg';
-import mypic2 from '../public/images/art-hauntington-jzY0KRJopEI-unsplash.jpg';
-import mypic3 from '../public/images/christian-buehner-6YQmQgcQ0VA-unsplash.jpg';
 import plus from '../public/images/plus.png';
 import styles from './Tile.module.css';
 import { shuffle } from 'lodash';
@@ -12,41 +9,10 @@ const Tile: React.FunctionComponent = ({ allDiems, diem, setCurrentDiem }) => {
     setCurrentDiem(diem);
   };
 
-  const pics = [mypic, mypic2, mypic2, mypic3, mypic3];
-  const date = diem.date;
-  const event = diem.title;
-
   return (
     <div className={styles.tile} onClick={divClickedHandler}>
-      <div className={styles.tile__profilePics_container}>
-
-        <div className={styles.tile__profilePic_plusSign}>
-          <Image src={plus} alt="Picture of the author" />
-        </div>
-
-        {shuffle(
-          diem.users
-            .map((el) => {
-              return (
-                <div key={el.id} className={styles.tile__profilePic}>
-                  <Image
-                src={el.userPhoto}
-                height="50"
-                width="50"
-                alt="Picture of the author"
-              />
-                </div>
-              );
-            })
-            .slice(0, 5)
-        )}
-
-      </div>
-
-      <div className={styles.tile__info}>
-        <h1>{event}</h1>
-        <h2>{date}</h2>
-      </div>
+      <h1>{diem.title}</h1>
+      <h2>{diem.date}</h2>
     </div>
   );
 };
