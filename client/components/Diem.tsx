2a87be9de11aabda6144ebcf3d3bcf4187d883d5
Alastair Fletcher <alastair.fletcher02@gmail.com>
2022-05-14 12:19:48 +0200

    add DiemInfoBar component to page

--- client/components/Diem.tsx
+++ client/components/Diem.tsx
@@ -1,18 +1,15 @@
 import React, { useEffect } from 'react';
 import Image from 'next/image';
 
-import mypic from '../public/images/amir-seilsepour-Pc0ToyoR5Xo-unsplash.jpg';
-import mypic2 from '../public/images/art-hauntington-jzY0KRJopEI-unsplash.jpg';
-import mypic3 from '../public/images/christian-buehner-6YQmQgcQ0VA-unsplash.jpg';
-import chat from '../public/images/chat.png';
-import calendar from '../public/images/calendar.png';
 import more from '../public/images/more.png';
 
 import styles from './Diem.module.css';
+import DiemInfoBar from './DiemInfoBar';
 import AddNewEvent from './AddNewEvent';
 import PopAddUsers from './PopAddUsers';
 import AddNewEventTest from './AddNewEventTest';
 import PopRemoveUsers from './PopRemoveUsers';
+
 const Diem: React.FunctionComponent = ({
   mainDiem,
   currentDiem,
@@ -20,21 +17,26 @@ const Diem: React.FunctionComponent = ({
   users,
 }) => {
   useEffect(() => {}, [currentDiem]);
-  const pics = [mypic, mypic2, mypic3];
   const date = currentDiem.date;
   const event = currentDiem.title;
 
   return (
     <>
       <div className={styles.diem}>
-        <div className={styles.diem__infobar}>
+        <DiemInfoBar
+          mainDiem={mainDiem}
+          currentDiem={currentDiem}
+          setCurrentDiem={setCurrentDiem}
+          users={users}
+        />
+        {/* <div className={styles.diem__infobar}>
           <div className={styles.tile__info}>
             <h1>{event}</h1>
             <h2>{date}</h2>
           </div>
           <Image src={chat} height="35" width="45" />
           <Image src={calendar} height="20" width="40" />
-        </div>
+        </div> */}
         <div>
           <div className={styles.diem__users}>
             <button type="button">
