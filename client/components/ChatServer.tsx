f2570b9603e0fbfc2845e5af94564ab740e2f084
Casey Christiansen <casey@littlerockfarm.com>
2022-05-17 17:56:33 +0200

    Modififying ChatServer component

--- client/components/ChatServer.tsx
+++ client/components/ChatServer.tsx
@@ -1,25 +1,47 @@
 import React, { useState, useEffect, useContext } from "react";
 import io from "socket.io-client";
-import getDiems from "../services/ApiServices";
-import updateDiemChatHistory from "../services/ApiServices";
+import getDiemById from "../services/ApiServices";
+
+import hooks from "../services/ApiServices";
 import Message from "./Message";
 import { SocketContext } from "../contexts/Socket";
 import { LoginContext } from "../contexts/Context";
 import styles from "./ChatServer.module.css";
+import e from "express";
+import { useForm } from "react-hook-form";
 
 const ChatServer: React.FunctionComponent = (props) => {
-  const [history, setHistory] = useState([]); //currentdiemchat history
+  //; //currentdiemchat history
   const socket = useContext(SocketContext);
   const { loginInfo } = useContext(LoginContext);
+  const { register, handleSubmit, reset } = useForm();
+  console.log(props.currentDiem.chatHistory, "CHAT HISTORY PROP");
 
-  const Form = () => {
-    const [mes, setMes] = React.useState({
-      content: "empty",
-      author: loginInfo.email,
-      timestamp: Date.now(),
-    });
-  };
-  //TODO setHistory(props.diem.chatHistory)
+  //On loading component, fetch chathistory from database
+
+  // const [history, setHistory] = useState([]);
+
+  // const [mes, setMes] = React.useState({
+  //   content: "empty",
+  //   author: loginInfo.email,
+  //   timestamp: Date.now(),
+  // });
+
+  //const [history, setHistory] = useState(getDiemById(props.currentDiem.id).chatHistory); //fetch updated chat history each time we load chatServer component
+
+  const [history, setHistory] = useState(props.currentDiem.chatHistory);
+
+  //setHistory(currentDiem.chatHistory);
+
+  // const mockData = [
+  //   { content: "This is a message", author: 1, timestamp: "Friday" },
+  //   { content: "A second message", author: 1, timestamp: "Wednesday" },
+  //   { content: "Yet another message", author: 2, timestamp: "Sunday" },
+  // ];
+
+  // const [history, setHistory] = useState(mockData);
+  //const [history, setHistory] = useState([]);
+  //setHistory((prev) => currentDiem.chatHistory)//TODO fetch the chat history data from the currently selected diem
 
   //Recieve an updated message
   socket.on("updatedMessages", (message) => {
@@ -28,56 +50,89 @@ const ChatServer: React.FunctionComponent = (props) => {
 
   useEffect(() => {}, [history]);
 
-  const mockData = [
-    { content: "This is a message", author: 1, timestamp: "Friday" },
-    { content: "A second message", author: 1, timestamp: "Wednesday" },
-    { content: "Yet another message", author: 2, timestamp: "Sunday" },
-  ];
+  //console.log(history, "HISTORY");
 
-  function handleSubmitMessage(
-    content: String,
-    author: String,
-    timestamp: String
-  ) {
-    updateDiemChatHistory(content, author, timestamp);
-    socket.emit("message", { content, author, timestamp });
+  // function handleSubmitMessage(e) {
+  //   e.preventDefault();
+  //   let time = Date.now();
+  //    //updateDiemChatHistory(e.value, loginInfo.email, time);
+  //   socket.emit("message", { e.value, loginInfo.email, time});
+  // }
+
+  function handleSubmitMessage(data: React.FormEvent<HTMLInputElement>) {
+    console.log(data, "LOGGIN E");
+    console.log(data.message, "input value");
+
+    let newMessage = {
+      message: "data.message",
+      author: "Email",
+      //author: loginInfo.email
+      timestamp: "String(Date.now())",
+    };
+
+    hooks.modifyDiemChatHistory(
+      newMessage.message,
+      3,
+
+      newMessage.author,
+      newMessage.timestamp
+    );
+
+    setHistory(
+      (prev) => [
+        ...prev,
+        { content: data.message, author: "ME", timestamp: String(Date.now()) },
+      ]
+
+      //{ content: e.value, author: loginInfo.name, timestamp: "" + Date.now() },
+
+      //{ content: e.value, author: loginInfo.name, timestamp: "" + Date.now() },  //TODO
+    );
+    reset({ message: "" });
   }
 
   return (
-    // <div>
-    //   <div className="justify-center items-center bg-white relative">
-    //     <div className={styles.message_container}>
-    //       {mockData.map((el) => {
-    //         return <Message message={el} />;
-    //       })}
-    //     </div>
-    //   </div>
-    //   <div className="w-screen h-screen flex justify-center items-center bg-white relative"></div>
-    // </div>
-    <div className={styles.form_contianer}>
-      <div className={styles.message_container}>
-        {mockData.map((el) => {
-          return <Message message={el} />;
-        })}
-      </div>
-      <div className={styles.form_container}>
-        <form id="loginForm" name="loginForm" className={styles.form}>
-          <label htmlFor="userLogin">login</label>
-          <input
-            type="text"
-            id="userLogin"
-            name="userLogin"
-            className="py-2 px-4 rounded"
-          />
+    <>
+      <div className={styles.form_contianer}>
+        {history &&
+          props.currentDiem(
+            <div className={styles.message_container}>
+              {props.currentDiem.chatHistory.map((el) => {
+                return <Message el={el} />;
+              })}
+            </div>
+          )}
 
-          <input
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          />
-        </form>
+        <div className={styles.form_container}>
+          <form
+            id="messages"
+            name="messages"
+            className={styles.form}
+            onSubmit={() => {
+              handleSubmit((data) => {
+                console.log(data, "DATA");
+                handleSubmitMessage(data);
+              });
+            }}
+          >
+            <label htmlFor="userLogin">Message Container</label>
+            <input
+              type="text"
+              id="inputValue"
+              name="inputValue"
+              className="py-2 px-4 rounded"
+              {...register("message")}
+              placeholder="enter message"
+            />
+
+            <input
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            />
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
-
 export default ChatServer;
